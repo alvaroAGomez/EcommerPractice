a502822972cf8279a32f792b8775f707f541f429
Alvaro <alvaro11122@gmail.com>
2022-11-05 21:05:23 -0300

    agregado de paginas y rutas base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  api_url:'https://young-sands-07814.herokuapp.com'
+  //api_url:'https://young-sands-07814.herokuapp.com'
+  api_url:'https://damp-spire-59848.herokuapp.com'
 
 };
